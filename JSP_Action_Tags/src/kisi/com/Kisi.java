1ec50125c220f5fcba2696cbe59a304397a9b6cb
s0207 <s0207@SBM-4030207.sbmdc.org.tr>
2019-09-25 13:21:40 +0300

    JSP Action Tags

---
+++ JSP_Action_Tags/src/kisi/com/Kisi.java
@@ -0,0 +1,19 @@
+package kisi.com;
+
+public class Kisi {
+	private String isim;
+	private String soyisim;
+	
+	public String getIsim() {
+		return isim;
+	}
+	public void setIsim(String isim) {
+		this.isim = isim;
+	}
+	public String getSoyisim() {
+		return soyisim;
+	}
+	public void setSoyisim(String soyisim) {
+		this.soyisim = soyisim;
+	}
+}
